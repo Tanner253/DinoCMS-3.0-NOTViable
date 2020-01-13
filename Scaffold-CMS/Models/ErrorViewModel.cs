61723f6a2cedfb1271eaeb59225e5fd2f2ab7efc
Tanner Percival <percivaltanner@gmail.com>
2020-01-13 09:55:25 -0800

    Initial Scaffold

---
+++ Scaffold-CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scaffold_CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
